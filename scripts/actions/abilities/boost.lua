35942f52be3761462120e7d19c6fe990b27278b0
Michael Everett <michae.everett@curology.com>
2024-05-07 13:42:17 -0700

    regen fun

--- scripts/actions/abilities/boost.lua
+++ scripts/actions/abilities/boost.lua
@@ -5,7 +5,6 @@
 -- Recast Time: 0:15
 -- Duration: 3:00
 -----------------------------------
-
 local abilityObject = {}
 
 abilityObject.onAbilityCheck = function(player, target, ability)
@@ -13,22 +12,19 @@ abilityObject.onAbilityCheck = function(player, target, ability)
 end
 
 abilityObject.onUseAbility = function(player, target, ability)
-    -- Apply Regen II effect
-    local regenDuration = 120 -- 2 minutes duration
-    local regenAmount = 2 -- Adjust Regen amount as needed
-    player:addStatusEffect(xi.effect.REGEN_II, regenAmount, 3, regenDuration, 0, 10, 1)
+    xi.job_utils.monk.useBoost(player, target, ability)
 
-    -- Add a 50% chance to grant haste
-    if math.random() <= 0.2 then
-        local hasteDuration = 180 -- Adjust duration as needed
-        player:addStatusEffect(xi.effect.HASTE, 30, 3, hasteDuration, 0, 10, 1)
+    -- Add a 50% chance to gain Regen
+    if math.random() <= 0.5 then
+        local regenDuration = 180 -- 3 minutes duration
+        local regenAmount = 2 -- Adjust Regen amount as needed
+        player:addStatusEffect(xi.effect.REGEN, regenAmount, 3, regenDuration, 0, 10, 1)
     end
 
-    -- Add a 10% chance to gain Regen status effect
-    if math.random() <= 0.5 then
-        local regenEffectDuration = 120 -- 2 minutes duration
-        local regenEffectAmount = 2 -- Adjust Regen amount as needed
-        player:addStatusEffect(xi.effect.REGEN, regenEffectAmount, 3, regenEffectDuration, 0, 10, 1)
+    -- Add a 20% chance to gain Haste
+    if math.random() <= 0.2 then
+        local hasteDuration = 180 -- 3 minutes duration
+        player:addStatusEffect(xi.effect.HASTE, 30, 3, hasteDuration, 0, 10, 1)
     end
 end
 

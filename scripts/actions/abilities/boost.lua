4c7ebac0e303829374a3c4462592faeba041b412
Michael Everett <michae.everett@curology.com>
2024-05-07 12:25:55 -0700

    changing to increase stats

--- scripts/actions/abilities/boost.lua
+++ scripts/actions/abilities/boost.lua
@@ -1,20 +1,30 @@
 local abilityObject = {}
 
-local abilityObject = {}
-
 abilityObject.onAbilityCheck = function(player, target, ability)
     return 0, 0
 end
 
 abilityObject.onUseAbility = function(player, target, ability)
-    -- Increase counter chance by 80%
-    local counterChanceIncrease = 0.8 -- 80% increase
-    player:addMod(xi.mod.COUNTER_CHANCE, counterChanceIncrease)
+    -- Add 20 evasion
+    player:addMod(xi.mod.EVASION, 20)
 
-    -- Set a timer to remove the counter chance increase after the ability duration
-    local abilityDuration = 60 -- Adjust duration as needed
+    -- Add 5 vitality
+    player:addMod(xi.mod.VIT, 5)
+
+    -- Add 5 strength
+    player:addMod(xi.mod.STR, 5)
+
+    -- Add 5 attack
+    player:addMod(xi.mod.ATTACK, 5)
+
+    -- Set a timer to remove the modifications after 1 minute
+    local abilityDuration = 60 -- 1 minute duration
     xi.timers.createTimer(player, "ability_duration", xi.timer.TYPE_ABILITY, abilityDuration, function()
-        player:addMod(xi.mod.COUNTER_CHANCE, -counterChanceIncrease) -- Remove the counter chance increase
+        -- Remove the modifications
+        player:addMod(xi.mod.EVASION, -20)
+        player:addMod(xi.mod.VIT, -5)
+        player:addMod(xi.mod.STR, -5)
+        player:addMod(xi.mod.ATTACK, -5)
     end)
 end
 

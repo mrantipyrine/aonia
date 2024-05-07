a9b161b19fc23529920823714c9c0d39c284e1c2
Michael Everett <michae.everett@curology.com>
2024-05-07 12:16:48 -0700

    testing boosts buff

--- scripts/actions/abilities/boost.lua
+++ scripts/actions/abilities/boost.lua
@@ -7,11 +7,11 @@ end
 abilityObject.onUseAbility = function(player, target, ability)
     xi.job_utils.monk.useBoost(player, target, ability)
 
-    -- Add a 10% chance to boost HP
-    if math.random() <= 0.1 then -- Change to 10% chance
-        local hpBoostDuration = 60 -- Adjust duration to 1 minute
-        local hpBoostAmount = 100 -- Adjust HP boost amount as needed
-        player:addStatusEffect(xi.effect.MAX_HP_BOOST, hpBoostAmount, 3, hpBoostDuration, 0, 10, 1)
+    -- Add a 10% chance to add blink
+    if math.random() <= 0.7 then -- Change to 10% chance
+        local blinkDuration = 60 -- Adjust duration to 1 minute
+        local blinkCharges = 3 -- Adjust blink charges as needed
+        player:addStatusEffect(xi.effect.BLINK, blinkCharges, 3, blinkDuration, 0, 10, 1)
     end
 end
 

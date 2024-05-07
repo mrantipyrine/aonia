bac43af9360a4bf7754574af0c937bf0c3ee47f1
Michael Everett <michae.everett@curology.com>
2024-05-07 14:09:19 -0700

    dodge change

--- scripts/actions/abilities/boost.lua
+++ scripts/actions/abilities/boost.lua
@@ -1,10 +1,3 @@
------------------------------------
--- Ability: Boost
--- Enhances user's next attack.
--- Obtained: Monk Level 5
--- Recast Time: 0:15
--- Duration: 3:00
------------------------------------
 local abilityObject = {}
 
 abilityObject.onAbilityCheck = function(player, target, ability)
@@ -12,15 +5,26 @@ abilityObject.onAbilityCheck = function(player, target, ability)
 end
 
 abilityObject.onUseAbility = function(player, target, ability)
-    xi.job_utils.monk.useBoost(player, target, ability)
+    -- Apply regen effect for 1 minute
+    local regenDuration = 60 -- 1 minute duration
+    local regenAmount = 2 -- Adjust regen amount as needed
+    player:addStatusEffect(xi.effect.REGEN, regenAmount, 3, regenDuration, 0, 10, 1)
 
-    -- Restore 80% of lost HP
+    -- Restore 80% of lost HP after 1 minute
     local lostHP = player:getMaxHP() - player:getHP()
     local hpToRestore = math.floor(lostHP * 0.8)
-    player:setHP(player:getHP() + hpToRestore)
 
-    -- Grant 500 TP
-    player:addTP(500)
+    local restoreHealth = function()
+        player:setHP(player:getHP() + hpToRestore)
+
+        -- Grant 500 TP after restoring health
+        player:addTP(500)
+    end
+
+    -- Schedule the restoration of health after 1 minute
+    xi.scheduler:schedule(restoreHealth, 60)
+
+    xi.job_utils.monk.useBoost(player, target, ability)
 end
 
 return abilityObject

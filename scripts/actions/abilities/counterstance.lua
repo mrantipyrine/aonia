1ebf3d5dee250ed59906cbb820afb328554f7150
Michael Everett <michae.everett@curology.com>
2024-05-07 11:41:56 -0700

    fixing error

--- scripts/actions/abilities/counterstance.lua
+++ scripts/actions/abilities/counterstance.lua
@@ -13,13 +13,24 @@ end
 
 local function adjustAttributes(player)
     -- Increase evasion by 50 and defense by 50
-    player:addMod(xi.mod.EVASION, 80)
-    player:addMod(xi.mod.DEFENSE, 80)
+    player:addMod(xi.mod.EVASION, 50)
+    player:addMod(xi.mod.DEFENSE, 50)
+end
+
+local function removeAttributes(player)
+    -- Remove the adjustments made to evasion and defense
+    player:addMod(xi.mod.EVASION, -50)
+    player:addMod(xi.mod.DEFENSE, -50)
 end
 
 abilityObject.onUseAbility = function(player, target, ability)
     -- Adjust attributes for Counterstance
     adjustAttributes(player)
+
+    -- Set up cleanup function to remove attribute adjustments after ability duration
+    xi.timers.createTimer(player, "counterstance_cleanup", xi.timer.TYPE_ABILITY, ability:getDuration(), function()
+        removeAttributes(player)
+    end)
 end
 
 return abilityObject

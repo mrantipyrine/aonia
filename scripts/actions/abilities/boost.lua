6aa639b6801a743e6db326cf798dcd428de1d7e9
Michael Everett <michae.everett@curology.com>
2024-05-07 12:13:58 -0700

    fixing boost

--- scripts/actions/abilities/boost.lua
+++ scripts/actions/abilities/boost.lua
@@ -1,20 +1,18 @@
 local abilityObject = {}
 
+abilityObject.onAbilityCheck = function(player, target, ability)
+    return 0, 0
+end
+
 abilityObject.onUseAbility = function(player, target, ability)
-    if player then
-        xi.job_utils.monk.useBoost(player, target, ability)
+    xi.job_utils.monk.useBoost(player, target, ability)
 
-        -- Calculate the HP boost amount based on player level
-        local playerLevel = player:getLevel()
-        local baseLevel = 5 -- Start scaling from level 5
-        local hpBoostAmount = (playerLevel - baseLevel + 1) * 50 -- Scale by 50 for every player level starting from level 5
-
-        -- Add a 10% chance to boost HP
-        if math.random() <= 0.1 then
-            local hpBoostDuration = 60 -- Adjust duration to 1 minute
-            player:addStatusEffect(xi.effect.MAX_HP_BOOST, hpBoostAmount, 3, hpBoostDuration, 0, 10, 1)
-        end
-    else
-        print("Error: Player object is nil.")
+    -- Add a 10% chance to boost HP
+    if math.random() <= 0.1 then -- Change to 10% chance
+        local hpBoostDuration = 60 -- Adjust duration to 1 minute
+        local hpBoostAmount = 100 -- Adjust HP boost amount as needed
+        player:addStatusEffect(xi.effect.MAX_HP_BOOST, hpBoostAmount, 3, hpBoostDuration, 0, 10, 1)
     end
 end
+
+return abilityObject

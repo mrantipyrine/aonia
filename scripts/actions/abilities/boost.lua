e63d77928307c11da997a2683ee7795093ab7152
Michael Everett <michae.everett@curology.com>
2024-05-07 13:29:16 -0700

    few changes

--- scripts/actions/abilities/boost.lua
+++ scripts/actions/abilities/boost.lua
@@ -16,12 +16,11 @@ abilityObject.onUseAbility = function(player, target, ability)
     -- Add 500 TP
     player:addTP(500)
 
-    -- Add a 50% chance to apply Stoneskin
+    -- Add a 50% chance to grant haste
     if math.random() <= 0.5 then
-        local stoneskinDuration = 60 -- Adjust duration to 1 minute
-        local stoneskinAmount = 200 -- Adjust Stoneskin amount as needed
-        player:addStatusEffect(xi.effect.STONESKIN, stoneskinAmount, 3, stoneskinDuration, 0, 10, 1)
-    end
+        local hasteDuration = 180 -- Adjust duration as needed
+        player:addStatusEffect(xi.effect.HASTE, 30, 3, hasteDuration, 0, 10, 1)
+    end 
 end
 
 return abilityObject
--- scripts/actions/abilities/dodge.lua
+++ scripts/actions/abilities/dodge.lua
@@ -16,8 +16,9 @@ abilityObject.onUseAbility = function(player, target, ability)
 
     -- Add a 50% chance to grant haste
     if math.random() <= 0.5 then
-        local hasteDuration = 180 -- Adjust duration as needed
-        player:addStatusEffect(xi.effect.HASTE, 30, 3, hasteDuration, 0, 10, 1)
+        local blinkDuration = 60 -- Adjust duration to 1 minute
+        local blinkCharges = 3 -- Adjust blink charges as needed
+        player:addStatusEffect(xi.effect.BLINK, blinkCharges, 3, blinkDuration, 0, 10, 1)
     end
 end 
 

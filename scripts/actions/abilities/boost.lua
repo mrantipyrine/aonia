df25991c70b5237e83b7a81e8d5bb88069176550
Michael Everett <michae.everett@curology.com>
2024-05-07 12:05:13 -0700

    fixing boost

--- scripts/actions/abilities/boost.lua
+++ scripts/actions/abilities/boost.lua
@@ -8,8 +8,8 @@ abilityObject.onUseAbility = function(player, target, ability)
     xi.job_utils.monk.useBoost(player, target, ability)
 
     -- Add a 10% chance to boost HP
-    if math.random() <= 0.5 then
-        local hpBoostDuration = 180 -- Adjust duration as needed
+    if math.random() <= 0.1 then -- Change to 10% chance
+        local hpBoostDuration = 60 -- Adjust duration to 1 minute
         local hpBoostAmount = 100 -- Adjust HP boost amount as needed
         player:addStatusEffect(xi.effect.MAX_HP_BOOST, hpBoostAmount, 3, hpBoostDuration, 0, 10, 1)
     end

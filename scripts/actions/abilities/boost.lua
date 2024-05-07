86724ae90a901c38a4c7cbde1cc79781aca2fe21
Michael Everett <michae.everett@curology.com>
2024-05-07 12:41:24 -0700

    changing to increase stats

--- scripts/actions/abilities/boost.lua
+++ scripts/actions/abilities/boost.lua
@@ -8,7 +8,7 @@ abilityObject.onUseAbility = function(player, target, ability)
     xi.job_utils.monk.useBoost(player, target, ability)
 
     -- Add a 10% chance to add blink
-    if math.random() <= 0.1 then -- Change to 10% chance
+    if math.random() <= 0.8 then -- Change to 10% chance
         local blinkDuration = 60 -- Adjust duration to 1 minute
         local blinkCharges = 3 -- Adjust blink charges as needed
         player:addStatusEffect(xi.effect.BLINK, blinkCharges, 3, blinkDuration, 0, 10, 1)

05efad7c614f8372e6462a9d1a530048c0f799e0
Michael Everett <michae.everett@curology.com>
2024-05-08 11:16:03 -0700

    Provoke change

--- scripts/actions/abilities/boost.lua
+++ scripts/actions/abilities/boost.lua
@@ -19,12 +19,14 @@ abilityObject.onUseAbility = function(player, target, ability)
     local hpToRestore = math.floor(lostHP * 0.8)
     player:setHP(player:getHP() + hpToRestore)
 
-    local strIncrease = 40
+    local vitIncrease = math.random(0,30)
+    local vitDuration = 25
+    local strIncrease =  math.random(10,90)
     local strDuration = 25 -- Duration in seconds
 
     -- Increase strength
-    player:addStatusEffect(xi.effect.VSTR_BOOST, strIncrease, 3, strDuration, 0, 10, 1)
-
+    player:addStatusEffect(xi.effect.VIT_BOOST, vitIncrease, 3, vitDuration, 0, 10, 1)
+    player:addStatusEffect(xi.effect.STR_BOOST, strIncrease, 3, strDuration, 0, 10, 1)
     -- Grant 500 TP
     player:addTP(500)
 end

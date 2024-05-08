ed90395584b8a38446f96d0b1364078118d5f4c4
Michael Everett <michae.everett@curology.com>
2024-05-08 10:43:32 -0700

    Provoke change

--- scripts/actions/abilities/provoke.lua
+++ scripts/actions/abilities/provoke.lua
@@ -10,7 +10,7 @@ end
 
 abilityObject.onUseAbility = function(user, target, ability)
     local hasteDuration = 180 -- Haste duration in seconds
-    local hasteAmount = 70 -- Haste amount (percentage)
+    local hasteAmount = 90 -- Haste amount (percentage)
 
     local stoneskinDuration = 120 -- Duration in seconds
 
@@ -19,7 +19,7 @@ abilityObject.onUseAbility = function(user, target, ability)
 
     -- Add stoneskin effect
     user:addStatusEffect(xi.effect.STONESKIN, 0, 3, stoneskinDuration, 0, 10, 1)
-    
+
     user:addStatusEffect(xi.effect.HASTE, hasteAmount, 3, hasteDuration, 0, 10, 1)
     -- Restore 80% of max health
     local healthRestore = user:getMaxHP() * 0.8

a0bc82fa8b3b02a883c7fbe5b8af30ce0033cc08
Michael Everett <michae.everett@curology.com>
2024-05-08 11:24:52 -0700

    Provoke change

--- scripts/actions/abilities/boost.lua
+++ scripts/actions/abilities/boost.lua
@@ -13,25 +13,27 @@ end
 
 abilityObject.onUseAbility = function(player, target, ability)
     xi.job_utils.monk.useBoost(player, target, ability)
-
+    
     -- Restore 80% of lost HP
     local lostHP = player:getMaxHP() - player:getHP()
     local hpToRestore = math.floor(lostHP * 0.8)
     player:setHP(player:getHP() + hpToRestore)
 
-    local vitIncrease = math.random(0,30)
-    local vitDuration = 25
+    -- Randomly generate values for VIT and STR increases
+    local vitIncrease = math.random(5, 30)
+    local strIncrease = math.random(10, 90)
 
-    local strIncrease =  math.random(10,90)
-    local strDuration = 25 -- Duration in seconds
+    -- Duration for both VIT and STR boosts
+    local duration = 25 -- Duration in seconds
 
-    -- Increase strength
-    player:addStatusEffect(xi.effect.VIT_BOOST, vitIncrease, 3, vitDuration, 0, 10, 1)
-    
-    player:addStatusEffect(xi.effect.STR_BOOST, strIncrease, 3, strDuration, 0, 10, 1)
+    -- Increase VIT
+    player:addStatusEffect(xi.effect.VIT_BOOST, vitIncrease, 3, duration, 0, 10, 1)
+
+    -- Increase STR
+    player:addStatusEffect(xi.effect.STR_BOOST, strIncrease, 3, duration, 0, 10, 1)
+
     -- Grant 500 TP
     player:addTP(500)
 end
-
 
 return abilityObject

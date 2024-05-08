8893f5ed256acdd4f84654e478433c958a5cd9bb
Michael Everett <michae.everett@curology.com>
2024-05-08 13:35:10 -0700

    typo

--- scripts/actions/abilities/defender.lua
+++ scripts/actions/abilities/defender.lua
@@ -8,12 +8,28 @@ abilityObject.onAbilityCheck = function(player, target, ability)
     return 0, 0
 end
 
+local function adjustAttributes(player)
+    -- Increase evasion by 50 and defense by 50
+    player:addMod(xi.mod.EVASION, 50)
+    player:addMod(xi.mod.DEFENSE, 50)
+end
+
+local function removeAttributes(player)
+    -- Remove the adjustments made to evasion and defense
+    player:addMod(xi.mod.EVASION, -50)
+    player:addMod(xi.mod.DEFENSE, -50)
+end
+
+
 abilityObject.onUseAbility = function(player, target, ability)
     xi.job_utils.warrior.useDefender(player, target, ability)
 
-    -- Increase evasion by 100
-    local mods = require("scripts.utils.mods") -- Import the mods table
-    target:addMod(mods.EVASION, 100)
-end
+    adjustAttributes(player)
+    
+    xi.timers.createTimer(player, "defemder_cleanup", xi.timer.TYPE_ABILITY, ability:getDuration(), function()
+        removeAttributes(player) end)
+
+   ---    adjustAttributes(player)
+   ---  xi.scheduler.scheduleFunction(removeAttributes, 120) -
 
 return abilityObject

e1513a91aa09000fb108d6d5c5e8f390546a74d3
Michael Everett <michae.everett@curology.com>
2024-05-08 10:31:15 -0700

    Provoke change

--- scripts/actions/abilities/provoke.lua
+++ scripts/actions/abilities/provoke.lua
@@ -8,21 +8,20 @@ abilityObject.onAbilityCheck = function(player, target, ability)
     return 0, 0
 end
 
-abilityObject.onUseAbility = function(player, target, ability)
-    local doubleAttackBonus = 1 -- Double Attack bonus
-    local doubleAttackDuration = 120 -- Duration in seconds
+abilityObject.onUseAbility = function(user, target, ability)
+    local storeTpBonus = 4 -- Store TP bonus
+    local storeTpDuration = 120 -- Duration in seconds
 
-    -- Add Double Attack +1 effect
+    -- Add Store TP +4 effect
     if player then
-        player:addStatusEffect(xi.effect.DOUBLE_ATTACK, doubleAttackBonus, 3, doubleAttackDuration, 0, 10, 1)
+        player:addStatusEffect(xi.effect.STORE_TP, storeTpBonus, 3, storeTpDuration, 0, 10, 1)
     else
         log('Player object is nil!')
     end
-   
+       
     -- Restore 80% of max health
-    local healthRestore = user:getMaxHP() * 0.8
-    user:setHP(user:getHP() + healthRestore)
-
+        local healthRestore = user:getMaxHP() * 0.8
+        user:setHP(user:getHP() + healthRestore)
 end
 
 return abilityObject

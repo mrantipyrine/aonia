be46ef9d0f3edefa664816964e465741c1f77ffe
Michael Everett <michae.everett@curology.com>
2024-05-07 21:01:53 -0700

    making dodge better

--- scripts/actions/abilities/dodge.lua
+++ scripts/actions/abilities/dodge.lua
@@ -17,9 +17,10 @@ abilityObject.onUseAbility = function(player, target, ability)
     local evasionDuration = 120 -- 2 minutes in seconds
     player:addStatusEffect(xi.effect.EVASION_BOOST, evasionIncrease, 3, evasionDuration, 0, 10, 1)
 
-    -- Enhance counter ability by +2
-    local counterIncrease = 2
-    player:addMod(xi.mod.COUNTER, counterIncrease)
+    -- Enhance counter ability by +5 for 2 minutes
+    local counterIncrease = 5
+    local counterDuration = 120 -- 2 minutes in seconds
+    player:addTempMod(xi.mod.COUNTER, counterIncrease, counterDuration)
 end 
 
 return abilityObject

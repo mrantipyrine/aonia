4dfc9fb9c757923b587d8fc350f2a72eeb3a83ee
Michael Everett <michae.everett@curology.com>
2024-05-08 16:28:41 -0700

    Aggressor update

--- scripts/actions/abilities/aggressor.lua
+++ scripts/actions/abilities/aggressor.lua
@@ -11,12 +11,15 @@ end
 abilityObject.onUseAbility = function(player, target, ability)
 
    -- Randomly generate values for VIT and STR increases
-   local strIncrease = math.random(50, 100)
+   local accIncrease = 50
    -- Duration for both VIT and STR boosts
    local duration = 180 -- Duration in seconds
 
-   -- Increase STR
-   player:addStatusEffect(xi.effect.STR_BOOST, strIncrease, 3, duration, 0, 10, 1)
+    if player:getMainJob() == xi.job.WAR then
+        accIncrease = math.random(50, 100) * 2
+        duration = math.random(180, 200) * 2
+    end   
+   player:addStatusEffect(xi.effect.ACCURACY_BOOST, accIncrease, 3, duration, 0, 10, 1)
 
     xi.job_utils.warrior.useAggressor(player, target, ability)
 end

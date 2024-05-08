79a197ee0d3dac4a633b7c1d353e4bbc269d2bb7
Michael Everett <michae.everett@curology.com>
2024-05-08 12:00:02 -0700

    zerk haste

--- scripts/actions/abilities/berserk.lua
+++ scripts/actions/abilities/berserk.lua
@@ -11,10 +11,10 @@ end
 abilityObject.onUseAbility = function(player, target, ability)
     xi.job_utils.warrior.useBerserk(player, target, ability)
 
-    -- Grant the attack bonus
-    -- local attackBonus = 30
-    -- local attackDuration = 150 -- 2 minutes and 30 seconds in seconds
-    -- player:addMod(xi.mod.ATTACK, attackBonus, attackDuration)
+    -- Grant the defense bonus
+    local defenseBonus = 30
+    local defenseDuration = 150 -- 2 minutes and 30 seconds in seconds
+    player:addMod(xi.mod.DEFENSE, defenseBonus, defenseDuration)
 
     local hasteDuration = 180 -- 3 minutes in seconds
     player:addStatusEffect(xi.effect.HASTE, 30, 3, hasteDuration, 0, 10, 1)

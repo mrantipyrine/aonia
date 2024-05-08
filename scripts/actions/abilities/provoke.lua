3baae69cbea6ff0134479b6646619829ff4665c5
Michael Everett <michae.everett@curology.com>
2024-05-08 10:11:34 -0700

    Provoke change

--- scripts/actions/abilities/provoke.lua
+++ scripts/actions/abilities/provoke.lua
@@ -26,14 +26,17 @@ abilityObject.onUseAbility = function(player, target, ability)
     local healthToRestore = math.floor(missingHealth * 0.8)
     player:addHP(healthToRestore)
 
-    -- Check the equipped weapon type
-    local mainHandEquipment = player:getEquipment(0) -- Main hand slot index is 0
-    if mainHandEquipment then
-        local weaponType = mainHandEquipment:getType()
-        if weaponType == 1 or weaponType == 2 then -- Axe: 1, Sword: 2
-            grantAttackBoost(player, weaponType)
-        elseif weaponType == 4 or weaponType == 6 then -- Great Axe: 4, Two Hand Sword: 6
-            grantHaste(player)
+    -- Check if the player is wielding an axe or sword
+    local mainHandSlot = player:getInventorySlot(0) -- Main hand slot index is 0
+    if mainHandSlot then
+        local mainHandItem = player:getItem(mainHandSlot)
+        if mainHandItem then
+            local weaponType = mainHandItem:getType()
+            if weaponType == 1 or weaponType == 2 then -- Axe: 1, Sword: 2
+                grantAttackBoost(player, weaponType)
+            elseif weaponType == 4 or weaponType == 6 then -- Great Axe: 4, Two Hand Sword: 6
+                grantHaste(player)
+            end
         end
     end
 end

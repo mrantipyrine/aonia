da321bbb7a73f1ca8d6df4e8c2695c18146622aa
Michael Everett <michae.everett@curology.com>
2024-05-08 12:25:31 -0700

    hp increase with berserk

--- scripts/actions/abilities/berserk.lua
+++ scripts/actions/abilities/berserk.lua
@@ -8,8 +8,8 @@ abilityObject.onUseAbility = function(player, target, ability)
     xi.job_utils.warrior.useBerserk(player, target, ability)
 
     local strIncrease = math.random(10, 30)
-    
-    local duration = 25 -- Duration in seconds
+
+    local duration = 300 -- Duration in seconds
 
     -- Increase STR
     player:addStatusEffect(xi.effect.STR_BOOST, strIncrease, 3, duration, 0, 10, 1)

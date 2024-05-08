1deb45ed8fe4335ff4298313c5f26f140313f0ba
Michael Everett <michae.everett@curology.com>
2024-05-08 11:35:19 -0700

    Provoke change

--- scripts/actions/abilities/berserk.lua
+++ scripts/actions/abilities/berserk.lua
@@ -10,6 +10,16 @@ end
 
 abilityObject.onUseAbility = function(player, target, ability)
     xi.job_utils.warrior.useBerserk(player, target, ability)
+
+        -- Grant the attack bonus
+        local attackBonus = 30
+        local attackDuration = 150 -- 2 minutes and 30 seconds in seconds
+        player:addMod(xi.mod.ATTACK, attackBonus)
+    
+        -- Apply the "Advancing March" effect
+        local marchDuration = 150 -- 2 minutes and 30 seconds in seconds
+        player:addStatusEffect(xi.effect.ADVANCING_MARCH, 0, 3, marchDuration, 0, 10, 1)
+    end
 end
 
 return abilityObject

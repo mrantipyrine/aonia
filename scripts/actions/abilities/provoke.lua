48aaa704c7015c602aa8a5fb416dba5e65943ad4
Michael Everett <michae.everett@curology.com>
2024-05-08 10:28:39 -0700

    Provoke change

--- scripts/actions/abilities/provoke.lua
+++ scripts/actions/abilities/provoke.lua
@@ -8,13 +8,13 @@ abilityObject.onAbilityCheck = function(player, target, ability)
     return 0, 0
 end
 
-abilityObject.onUseAbility = function(user, target, ability)
+abilityObject.onUseAbility = function(player, target, ability)
     local doubleAttackBonus = 1 -- Double Attack bonus
     local doubleAttackDuration = 120 -- Duration in seconds
 
     -- Add Double Attack +1 effect
-    user:addStatusEffect(xi.effect.DOUBLE_ATTACK, doubleAttackBonus, 3, doubleAttackDuration, 0, 10, 1)
-       
+    player:addStatusEffect(xi.effect.DOUBLE_ATTACK, doubleAttackBonus, 3, doubleAttackDuration, 0, 10, 1)
+   
     -- Restore 80% of max health
     local healthRestore = user:getMaxHP() * 0.8
     user:setHP(user:getHP() + healthRestore)

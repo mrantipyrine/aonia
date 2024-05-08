6a8629572e1eeea22e176579861d79511cef10b3
Michael Everett <michae.everett@curology.com>
2024-05-08 10:53:49 -0700

    Provoke change

--- scripts/actions/abilities/provoke.lua
+++ scripts/actions/abilities/provoke.lua
@@ -13,7 +13,7 @@ abilityObject.onUseAbility = function(user, target, ability)
     local attackDuration = 25 -- Duration in seconds
 
     -- Increase attack power
-    player:addMod(xi.mod.ATTACK, attackIncrease)
+    user:addMod(xi.mod.ATTACK, attackIncrease)
 
     -- Apply Stoneskin
     local stoneskinAmount = 100 -- Adjust Stoneskin amount as needed
@@ -21,10 +21,10 @@ abilityObject.onUseAbility = function(user, target, ability)
     player:addStatusEffect(xi.effect.STONESKIN, stoneskinAmount, 3, stoneskinDuration, 0, 10, 1)
 
     -- Set up a timer to remove the attack increase after the duration
-    xi.timers.createTimer(player, "ability_duration", xi.timer.TYPE_ABILITY, attackDuration, function()
-        player:addMod(xi.mod.ATTACK, -attackIncrease)
+    xi.timers.createTimer(user, "ability_duration", xi.timer.TYPE_ABILITY, attackDuration, function()
+        user:addMod(xi.mod.ATTACK, -attackIncrease)
     end)
-    
+
     local healthRestore = user:getMaxHP() * 0.8
     user:setHP(user:getHP() + healthRestore)
 

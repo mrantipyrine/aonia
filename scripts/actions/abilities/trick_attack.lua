b0ebbadf8787e1d45f1c70f23e7993c38a94197b
Michael Everett <michae.everett@curology.com>
2024-05-09 10:53:31 -0700

    hide?

--- scripts/actions/abilities/trick_attack.lua
+++ scripts/actions/abilities/trick_attack.lua
@@ -2,6 +2,10 @@
 -- Ability: Trick Attack
 -- Deals extra damage when striking from behind a party or alliance member.
 -- Obtained: Thief Level 30
+-----------------------------------
+-- Ability: Trick Attack
+-- Deals extra damage when striking from behind a party or alliance member.
+-- Obtained: Thief Level 30
 -- Recast Time: 1:00
 -- Duration: 1:00
 -----------------------------------
@@ -12,7 +16,12 @@ abilityObject.onAbilityCheck = function(player, target, ability)
 end
 
 abilityObject.onUseAbility = function(player, target, ability)
+    if player:getMainJob() == xi.job.THF then
+        xi.job_utils.thief.useHide(player, target, ability)
+    end 
+    
     xi.job_utils.thief.useTrickAttack(player, target, ability)
 end
 
 return abilityObject
+

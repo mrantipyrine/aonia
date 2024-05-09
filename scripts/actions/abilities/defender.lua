bf12dc2af59004854783235eb2fb34f8c089a03b
Michael Everett <michae.everett@curology.com>
2024-05-08 21:27:47 -0700

    adding HP to defender

--- scripts/actions/abilities/defender.lua
+++ scripts/actions/abilities/defender.lua
@@ -18,7 +18,7 @@ abilityObject.onUseAbility = function(player, target, ability)
     if player:getMainJob() == xi.job.WAR then
         maxHP = math.random(100, 200) * 2 
         --target:delStatusEffect(xi.effect.MAX_HP_BOOST) -- TODO: confirm which versions of HP boost mantra can overwrite
-        target:addStatusEffect(xi.effect.MAX_HP_BOOST, maxHp, 0, maxHPDuration)
+        player:addStatusEffect(xi.effect.MAX_HP_BOOST, 200, 0, 0)
     end 
 
     --player:addStatusEffect(xi.effect.EVASION_BOOST, evasionIncrease, 3, evasionDuration, 0, 10, 1)

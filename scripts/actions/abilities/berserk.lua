68517ae92b0cbd9744865c7ac5f42673fa52343e
Michael Everett <michae.everett@curology.com>
2024-05-08 21:44:37 -0700

    berserk

--- scripts/actions/abilities/berserk.lua
+++ scripts/actions/abilities/berserk.lua
@@ -7,28 +7,30 @@ local abilityObject = {}
 abilityObject.onUseAbility = function(player, target, ability)
     xi.job_utils.warrior.useBerserk(player, target, ability)
 
-    -- STR Increase 
-    local strIncrease = 10
-    -- Duration in Seconds
-    local duration = 300 
+    strIncrease = math.random(10, 20)
+    accIncrease = math.random(30, 100)
+
+    duration = 180
 
     -- Increase if WAR is main job
     if player:getMainJob() == xi.job.WAR then
-        strIncrease = math.random(30, 50) * 2
+        strIncrease = strIncrease * 4
+        accIncrease = accIncrease * 3
     end 
-    -- Increase STR
-    player:addStatusEffect(xi.effect.STR_BOOST, strIncrease, 3, duration, 0, 10, 1)
 
+    -- Increase STR / Acc
+    player:addStatusEffect(xi.effect.STR_BOOST, strIncrease, 3, duration, 0, 10, 1)
+    player:addMod(xi.mod.ACC, accIncrease, 3, duration, 0, 10, 1)
     
     -- Apply the Haste effect
-    local regenDuration = 60 -- 1 minutes in seconds
+   -- local regenDuration = 60 -- 1 minutes in seconds
 
-    if player:getMainJob() == xi.job.WAR then
-        regenDuration = regenDuration * 10
-    end
+    -- if player:getMainJob() == xi.job.WAR then
+    --    regenDuration = regenDuration * 10
+    --end
 
     -- Incrase Medidate
-    player:addStatusEffect(xi.effect.MEDITATE, 15, 6, regenDuration, 0, 10, 1)
+     ---player:addStatusEffect(xi.effect.MEDITATE, 15, 6, regenDuration, 0, 10, 1)
 end
 
 return abilityObject
--- scripts/actions/abilities/defender.lua
+++ scripts/actions/abilities/defender.lua
@@ -4,7 +4,6 @@
 -----------------------------------
 local abilityObject = {}
 
-
 abilityObject.onAbilityCheck = function(player, target, ability)
     return 0, 0
 end
@@ -12,16 +11,19 @@ end
 abilityObject.onUseAbility = function(player, target, ability)
     xi.job_utils.warrior.useDefender(player, target, ability)
 
-    local maxHP = 100
-    local maxHPDuration = 60
+    local maxHP = 55
+    local maxHPDuration = 120
 
     if player:getMainJob() == xi.job.WAR then
-        maxHP = math.random(100, 200) * 2 
-        --target:delStatusEffect(xi.effect.MAX_HP_BOOST) -- TODO: confirm which versions of HP boost mantra can overwrite
-        player:addStatusEffect(xi.effect.MAX_HP_BOOST, 200, 0, 0)
-    end 
+        maxHP = maxHP * 2
+        maxHPDuration = maxHPDuration * 2
+    end
+        
+    player:addStatusEffect(xi.effect.MAX_HP_BOOST, maxHP, 1, maxHPDuration)
 
-    --player:addStatusEffect(xi.effect.EVASION_BOOST, evasionIncrease, 3, evasionDuration, 0, 10, 1)
+    local lostHP = player:getMaxHP() - player:getHP()
+    local hpToRestore = math.floor(lostHP * 0.8)
+    player:setHP(player:getHP() + hpToRestore)
 
 end
 

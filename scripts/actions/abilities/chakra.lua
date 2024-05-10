f1c82069f8fb63d3bdd9771791f433b48387ed4c
Michael Everett <michae.everett@curology.com>
2024-05-09 22:01:19 -0700

    Fixing chakra

--- scripts/actions/abilities/chakra.lua
+++ scripts/actions/abilities/chakra.lua
@@ -6,15 +6,16 @@ end
 
 abilityObject.onUseAbility = function(player, target, ability)
     
-    attackIncrease = 50
-    attackDuration = 120 
+    -- local attackIncrease = 50
+    -- local attackDuration = 120 
 
     if player:getMainJob() == xi.job.MNK then
-       attackIncrease = attackIncrease * 3
-       attackDuration = attackDuration * 3
+        player:addStatusEffect(xi.effect.MIGHTY_STRIKES, 1, 0, 0)
+        Timer.after(20, player:delStatusEffect(xi.effect.HUNDRED_FISTS))
     end  
 
-    player:addMod(xi.mod.ATTACK, attackIncrease, 3, attackDuration, 0, 10, 1)
+    player:addMod(xi.mod.ATT, 50, 120)
+    
                                               
     return xi.job_utils.monk.useChakra(player, target, ability)
 end

23e25cddefdfa78a8e1c1ddce3c415835ee04300
Michael Everett <michae.everett@curology.com>
2024-05-14 14:19:06 -0700

    more hp and less evasion for boost

--- scripts/actions/abilities/dodge.lua
+++ scripts/actions/abilities/dodge.lua
@@ -12,27 +12,25 @@ abilityObject.onAbilityCheck = function(player, target, ability)
 end
 
 abilityObject.onUseAbility = function(player, target, ability)
-    -- Increase evasion by 50 for 2 minutes
+    -- Set base evasion increase and duration
     local evasionIncrease = 50
     local duration = 120
 
+    -- Increase evasion and duration if main job is MNK
     if player:getMainJob() == xi.job.MNK then
         evasionIncrease = 100
         duration = 240
+
+        -- Increase Max HP and restore 80% missing HP if main job is MNK
+        local maxHPBoost = player:getMaxHP() * math.random(2, 3)
+        player:addStatusEffect(xi.effect.MAX_HP_BOOST, maxHPBoost, 1, duration)
+        local lostHP = player:getMaxHP() - player:getHP()
+        local hpToRestore = math.floor(lostHP * 0.8)
+        player:setHP(player:getHP() + hpToRestore)
     end
 
     -- Apply evasion boost
     player:addStatusEffect(xi.effect.EVASION_BOOST, evasionIncrease, 3, duration)
-
-    -- Increase Max HP and restore 80% missing if main job is MNK
-    if player:getMainJob() == xi.job.MNK then
-        local maxHPBoost = player:getHP() * 2
-        
-        player:addStatusEffect(xi.effect.MAX_HP_BOOST = player:getHP() * math.random(2, 3), maxHPBoost, 1, duration)
-        local lostHP = player:getHP() - player:getHP()
-        local hpToRestore = math.floor(lostHP * 0.8)
-        player:setHP(player:getHP() + hpToRestore)
-    end
 
     -- Trigger the Dodge ability
     xi.job_utils.monk.useDodge(player, target, ability)

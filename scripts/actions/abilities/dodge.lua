97b6da3ac4f90618807d3cedf78622bdd562d63a
Michael Everett <michae.everett@curology.com>
2024-05-14 12:42:15 -0700

    dodge update

--- scripts/actions/abilities/dodge.lua
+++ scripts/actions/abilities/dodge.lua
@@ -12,25 +12,24 @@ abilityObject.onAbilityCheck = function(player, target, ability)
 end
 
 abilityObject.onUseAbility = function(player, target, ability)
-    -- Set default evasion increase and duration
-    local evasionIncrease = player:getMainLvl()
+    -- Increase evasion by 50 for 2 minutes
+
+    local evasionIncrease = player:getMainLvl() * 1 
     local evasionDuration = 120
     
-    -- Adjust values if main job is MNK
     if player:getMainJob() == xi.job.MNK then
-        evasionIncrease = player:getMainLvl() * 2
-        evasionDuration = 240
         
-        -- Add counter boost status effect
-        local counterIncrease = evasionIncrease
-        local counterDuration = evasionDuration
-        player:addStatusEffect(xi.effect.DRAIN_SAMBA, counterIncrease, 3, counterDuration, 0, 10, 1)
+        evasionIncrease = getMainLvl() * 2
+        evasionDuration = 240
+
+        local counterIncrease = player:getMainJob() * 10
+        local counterDuration = 240
+
+        player:addStatusEffect(xi.effect.COUNTER_BOOST, counterIncrease, 3, counterDuration, 0, 10, 1)
     end 
 
-    -- Add evasion boost status effect
-     player:addStatusEffect(xi.effect.EVASION_BOOST, evasionIncrease, 3, evasionDuration, 0, 10, 1)
+    player:addStatusEffect(xi.effect.EVASION_BOOST, evasionIncrease, 3, evasionDuration, 0, 10, 1)
 
-    -- Use the Dodge ability
     xi.job_utils.monk.useDodge(player, target, ability)
 end 
 

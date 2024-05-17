44495d3e5f42f887ea49c845b818cc062767e5f6
Michael Everett <michae.everett@curology.com>
2024-05-16 22:08:00 -0700

    damage adjustment

--- scripts/actions/abilities/footwork.lua
+++ scripts/actions/abilities/footwork.lua
@@ -16,8 +16,8 @@ abilityObject.onUseAbility = function(player, target, ability)
     local doubleAttdmg = player:getMainLvl() 
     local duration = 120 
     local attack = player:getMainLvl() * 5
-    player:delMod(xi.mod.DOUBLE_ATTACK, doubleAtt, 3, duration, 0, 10, 1)
-    player:delMod(xi.mod.DOUBLE_ATTACK_DMG, doubleAttdmg, 3, duration, 0, 10, 1)
+    player:addMod(xi.mod.DOUBLE_ATTACK, doubleAtt, 3, duration, 0, 10, 1)
+    player:addMod(xi.mod.DOUBLE_ATTACK_DMG, doubleAttdmg, 3, duration, 0, 10, 1)
     player:delMod(xi.mod.ATT, attack, 3, duration, 0, 10, 1)
     
     xi.job_utils.monk.useFootwork(player, target, ability)
--- scripts/actions/abilities/hide.lua
+++ scripts/actions/abilities/hide.lua
@@ -22,9 +22,9 @@ abilityObject.onUseAbility = function(player, target, ability)
         local doubleAttdmg = player:getMainLvl() 
         local duration = 120 
         local attack = player:getMainLvl() * 5
-        player:delMod(xi.mod.DOUBLE_ATTACK, doubleAtt, 3, duration, 0, 10, 1)
-        player:delMod(xi.mod.DOUBLE_ATTACK_DMG, doubleAttdmg, 3, duration, 0, 10, 1)
-        player:delMod(xi.mod.ATT, attack, 3, duration, 0, 10, 1)
+        player:addMod(xi.mod.DOUBLE_ATTACK, doubleAtt, 3, duration, 0, 10, 1)
+        player:addMod(xi.mod.DOUBLE_ATTACK_DMG, doubleAttdmg, 3, duration, 0, 10, 1)
+        player:addMod(xi.mod.ATT, attack, 3, duration, 0, 10, 1)
     end
      
     xi.job_utils.thief.useHide(player, target, ability)

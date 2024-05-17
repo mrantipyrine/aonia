5b246f81769c5b50dee124ca6579f8b12d8a8084
Michael Everett <michae.everett@curology.com>
2024-05-16 20:22:07 -0700

    damage adjustment

--- scripts/actions/abilities/footwork.lua
+++ scripts/actions/abilities/footwork.lua
@@ -16,9 +16,9 @@ abilityObject.onUseAbility = function(player, target, ability)
     local doubleAttdmg = player:getMainLvl() / 2 
     local duration = 120 
     local attack = player:getMainLvl() * 5
-    player:addMod(xi.mod.DOUBLE_ATTACK, doubleAtt, 3, duration, 0, 10, 1)
-    player:addMod(xi.mod.DOUBLE_ATTACK_DMG, doubleAttdmg, 3, duration, 0, 10, 1)
-    player:addMod(xi.mod.ATT, attack, 3, duration, 0, 10, 1)
+    player:delMod(xi.mod.DOUBLE_ATTACK, doubleAtt, 3, duration, 0, 10, 1)
+    player:delMod(xi.mod.DOUBLE_ATTACK_DMG, doubleAttdmg, 3, duration, 0, 10, 1)
+    player:delMod(xi.mod.ATT, attack, 3, duration, 0, 10, 1)
     
     xi.job_utils.monk.useFootwork(player, target, ability)
 end

5100e5bf0d439d3365aa9f0f6986a284e511d71f
Michael Everett <michae.everett@curology.com>
2024-05-17 16:18:49 -0700

    warrior tuning

--- scripts/actions/abilities/defender.lua
+++ scripts/actions/abilities/defender.lua
@@ -21,15 +21,15 @@ abilityObject.onUseAbility = function(player, target, ability)
     end
     
 
-    player:addStatusEffect(xi.effect.REGAIN, regainAmount, 3, regainDuration, 0)
+    player:addStatusEffect(xi.mod.DEF, 100, 3, regainDuration, 0)
     player:addStatusEffect(xi.effect.REGEN, regenAmount, 3, regenDuration, 0, 10, 1)
 
     -- Increase Max HP and Restore 80% missing  
-    --layer:addStatusEffect(xi.effect.MAX_HP_BOOST, maxHP, 1, maxHPDuration)
+    layer:addStatusEffect(xi.effect.MAX_HP_BOOST, maxHP, 1, maxHPDuration)
     
-    --local lostHP = player:getMaxHP() - player:getHP()
-    --local hpToRestore = math.floor(lostHP * 0.8)
-    --player:setHP(player:getHP() + hpToRestore)
+    local lostHP = player:getMaxHP() - player:getHP()
+    local hpToRestore = math.floor(lostHP * 0.8)
+    player:setHP(player:getHP() + hpToRestore)
 
 end
 

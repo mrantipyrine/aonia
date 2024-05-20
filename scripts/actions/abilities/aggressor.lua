68ddc8d0cca5734a6a82945bf24a805da225e3ee
Michael Everett <michae.everett@curology.com>
2024-05-19 20:54:49 -0700

    warrior tuning

--- scripts/actions/abilities/aggressor.lua
+++ scripts/actions/abilities/aggressor.lua
@@ -11,11 +11,22 @@ end
 
 abilityObject.onUseAbility = function(player, target, ability)
 
-    regainAmount = math.random(10, 25)
-    regainDuration = 60
-
+    local duration = 180
+    local attIncrease = player:getMainLvl() / 4 
+    local tripleAtt = player:getMainLvl() / 8 
+    local tripleDmg = player:getMainLvl() / 6
     
-    player:addStatusEffect(xi.effect.REGAIN, regainAmount, regainDuration, 0)
+    if player:getMainJob() == xi.job.WAR then
+        attIncrease = player:getMainLvl()
+        tripleAtt = player:getMainLvl() 
+        tripleDmg = player:getMainLvl() / 2
+        duration = 290
+    end 
+
+    -- Increase ATT + Double Attack 
+    player:addMod(xi.mod.ATT, attIncrease, 3, duration, 0, 10, 1)
+    player:addMod(xi.mod.TRIPLE_ATTACK, doubleAtt, 3, duration, 0, 10, 1)
+    player:addMod(xi.mod.TRIPLE_ATTACK_DMG, doubleDmg, 3, duration, 0, 10, 1)
 
     xi.job_utils.warrior.useAggressor(player, target, ability)
 end

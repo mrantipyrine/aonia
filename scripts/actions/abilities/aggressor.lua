a2bc17a3b65aabe10ef4c437f12956aa7013d121
Michael Everett <michae.everett@curology.com>
2024-05-21 17:40:52 -0700

    warrior tuning

--- scripts/actions/abilities/aggressor.lua
+++ scripts/actions/abilities/aggressor.lua
@@ -11,22 +11,34 @@ end
 
 abilityObject.onUseAbility = function(player, target, ability)
 
-    local duration = 180
-    local attIncrease = player:getMainLvl() / 4 
-    local tripleAtt = player:getMainLvl() / 8 
-    local tripleDmg = player:getMainLvl() / 6
+
+    local equippedWeaponType = player:getEquippedWeaponType()
     
-    if player:getMainJob() == xi.job.WAR then
-        attIncrease = player:getMainLvl()
-        tripleAtt = player:getMainLvl() 
-        tripleDmg = player:getMainLvl() / 2
-        duration = 290
+    -- Check if the equipped weapon type is Greataxe (assuming 11 is Greataxe)
+    if equippedWeaponType ~= 11 then
+
+        return 0, 0, false, 0 -- Return early if the player does not have a Greataxe equipped
+    end
+
+    local duration = 290
+
+    local attIncrease = player:getMainLvl()
+
+    if equippedWeaponType then 
+        player:addMod(xi.mod.TRIPLE_ATTACK, player:getMainLvl(), 3, duration, 0, 10, 1)
+        player:addMod(xi.mod.TRIPLE_ATTACK_DMG, player:getMainLvl() / 2 , 3, duration, 0, 10, 1)
+        player:addStatusEffect(xi.effect.HASTE, 60, 3, duration, 0, 10, 1)
+    else
+        player:addStatusEffect(xi.effect.ACC, 60, 3, duration, 0, 10, 1)
+        player:addMod(xi.mod.DOUBLE_ATTACK, player:getMainLvl(), 3, duration, 0, 10, 1)
+        player:addMod(xi.mod.DOUBLE_ATTACK_DMG, player:getMainLvl() / 2 , 3, duration, 0, 10, 1)
+    end 
+    
+
     end 
 
     -- Increase ATT + Double Attack 
     player:addMod(xi.mod.ATT, attIncrease, 3, duration, 0, 10, 1)
-    player:addMod(xi.mod.TRIPLE_ATTACK, doubleAtt, 3, duration, 0, 10, 1)
-    player:addMod(xi.mod.TRIPLE_ATTACK_DMG, doubleDmg, 3, duration, 0, 10, 1)
 
     xi.job_utils.warrior.useAggressor(player, target, ability)
 end

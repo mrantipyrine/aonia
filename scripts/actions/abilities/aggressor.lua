3fdec06fdb7a0028ec2f7e8609b65094a1879f93
Michael Everett <michae.everett@curology.com>
2024-05-22 16:07:03 -0700

    warrior tuning

--- scripts/actions/abilities/aggressor.lua
+++ scripts/actions/abilities/aggressor.lua
@@ -10,23 +10,27 @@ abilityObject.onAbilityCheck = function(player, target, ability)
 end
 
 abilityObject.onUseAbility = function(player, target, ability)
-    local mainLvl = player:getMainLvl()
-    local duration = 180
-    local accIncrease = mainLvl 
-    local attIncrease = mainLvl
-    local weaponType = player:getWeaponSkillType(xi.slot.MAIN)
 
-    if weaponType == xi.skill.GREAT_AXE or weaponType == xi.skill.GREAT_SWORD then
-        player:addMod(xi.mod.QUAD_ATTACK, mainLvl / 4)
-        player:addMod(xi.mod.QUAD_ATTACK_DMG, mainLvl / 4)
-        player:addStatusEffect(xi.effect.HASTE, 60, 0, duration)
+    local tripleAtt = player:getMainLvl() / 4
+    local tripleAtt  = player:getMainLvl() / 4
+    local quadAttdmg  = player:getMainLvl() / 4
+    local quadAttdmg =  player:getMainLvl() /4
+    local accIncrease = player:getMainLvl()
+    local attIncrease = player:getMainLvl()
+    local duration = 180
+
+    if player:getWeaponSkillType(xi.slot.MAIN) == xi.skill.GREAT_AXE orplayer:getWeaponSkillType(xi.slot.MAIN) == xi.skill.GREAT_SWORD then
+        player:addMod(xi.mod.QUAD_ATTACK, player:getMainLvl() /2 , 3, duration, 0, 10, 1)
+        player:addMod(xi.mod.QUAD_ATTACK_DMG, player:getMainLvl() / 2 , 3, duration, 0, 10, 1)
+        player:addStatusEffect(xi.effect.HASTE, 60, 3, duration, 0, 10, 1)
     else
-        player:addMod(xi.mod.ACC, accIncrease)
-        player:addMod(xi.mod.TRIPLE_ATTACK, mainLvl / 4)
-        player:addMod(xi.mod.TRIPLE_ATTACK_DMG, mainLvl / 4)
+        player:addMod(xi.mod.ACC, accIncrease, 3, duration, 0, 10, 1)
+        player:addMod(xi.mod.TRIPLE_ATTACK, player:getMainLvl() /2 , 3, duration, 0, 10, 1)
+        player:addMod(xi.mod.TRIPLE_ATTACK_DMG, player:getMainLvl() / 2 , 3, duration, 0, 10, 1)
     end 
 
-    player:addMod(xi.mod.ATT, attIncrease)
+    -- Increase ATT + Double Attack 
+    player:addMod(xi.mod.ATT, attIncrease, 3, duration, 0, 10, 1)
 
     xi.job_utils.warrior.useAggressor(player, target, ability)
 end

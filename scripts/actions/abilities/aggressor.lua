2b87437b7c6527665cfd95c6cc444f5d6d93dd89
Michael Everett <michae.everett@curology.com>
2024-05-22 16:12:10 -0700

    warrior tuning

--- scripts/actions/abilities/aggressor.lua
+++ scripts/actions/abilities/aggressor.lua
@@ -11,22 +11,18 @@ end
 
 abilityObject.onUseAbility = function(player, target, ability)
 
-    --local tripleAtt = player:getMainLvl() / 4
-    --local tripleAttdmg  = player:getMainLvl() / 4
-    --local quadAtt  = player:getMainLvl() / 4
-    --local quadAttdmg =  player:getMainLvl() /4
-    --local accIncrease = player:getMainLvl()
+    local accIncrease = player:getMainLvl()
     local attIncrease = player:getMainLvl()
     local duration = 180
 
     if player:getWeaponSkillType(xi.slot.MAIN) == xi.skill.GREAT_AXE orplayer:getWeaponSkillType(xi.slot.MAIN) == xi.skill.GREAT_SWORD then
-        player:addMod(xi.mod.QUAD_ATTACK, player:getMainLvl() /2 , 3, duration, 0, 10, 1)
-        player:addMod(xi.mod.QUAD_ATTACK_DMG, player:getMainLvl() / 2 , 3, duration, 0, 10, 1)
+        player:addMod(xi.mod.TRIPLE_ATTACK, player:getMainLvl() /2 , 3, duration, 0, 10, 1)
+        player:addMod(xi.mod.TRIPLE_ATTACK_DMG, player:getMainLvl() / 2 , 3, duration, 0, 10, 1)
         player:addStatusEffect(xi.effect.HASTE, 60, 3, duration, 0, 10, 1)
     else
         player:addMod(xi.mod.ACC, accIncrease, 3, duration, 0, 10, 1)
-        player:addMod(xi.mod.TRIPLE_ATTACK, player:getMainLvl() /2 , 3, duration, 0, 10, 1)
-        player:addMod(xi.mod.TRIPLE_ATTACK_DMG, player:getMainLvl() / 2 , 3, duration, 0, 10, 1)
+        player:addMod(xi.mod.DOUBLE_ATTACK, player:getMainLvl() / 2, 3, duration, 0, 10, 1)
+        player:addMod(xi.mod.DOUBLE_ATTACK_DMG, player:getMainLvl() / 2 , 3, duration, 0, 10, 1)
     end 
 
     -- Increase ATT + Double Attack 

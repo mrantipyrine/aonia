814dcf3e0e2491f963372db3730d41bb285d4816
Michael Everett <michae.everett@curology.com>
2024-06-01 21:04:58 -0700

    stone spell effect

--- scripts/actions/spells/black/stone.lua
+++ scripts/actions/spells/black/stone.lua
@@ -8,6 +8,33 @@ spellObject.onMagicCastingCheck = function(caster, target, spell)
 end
 
 spellObject.onSpellCast = function(caster, target, spell)
+    local duration = 180
+    local player = caster -- Assuming 'caster' is the player object
+
+    local mainJob = player:getMainJob()
+    local subJob = player:getSubJob()
+    local mainLvl = player:getMainLvl()
+
+    local effect
+    local power
+
+    if mainJob == xi.job.RDM then
+        effect = xi.effect.ENSTONE
+        power = math.random(1, mainLvl)
+    elseif subJob == xi.job.RDM then
+        effect = xi.effect.ENSTONE
+        power = math.random(1, mainLvl / 4)
+    elseif mainJob == xi.job.BLM then
+        effect = xi.effect.STONESKIN
+        power = math.random(1, mainLvl)
+    elseif subJob == xi.job.BLM then
+        effect = xi.effect.STONESKIN
+        power = math.random(1, mainLvl / 4)
+    else
+        return
+    end
+
+    player:addStatusEffect(effect, power, 3, duration, 0, 10, 1)
     return xi.spells.damage.useDamageSpell(caster, target, spell)
 end
 

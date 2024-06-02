73a0d520da49774b1f189d1a211be659417ed37b
Michael Everett <michae.everett@curology.com>
2024-06-01 21:19:18 -0700

    stone spell effect

--- scripts/actions/spells/black/stone.lua
+++ scripts/actions/spells/black/stone.lua
@@ -39,8 +39,8 @@ spellObject.onSpellCast = function(caster, target, spell)
     
     
     if mainJob == xi.job.BLM and math.random() <= 0.99 then
-       xi.spells.damage.useDamageSpell(caster, target, spell)
-       xi.spells.damage.useDamageSpell(caster, target, spell)
+       return xi.spells.damage.useDamageSpell(caster, target, spell) * 2
+       -- xi.spells.damage.useDamageSpell(caster, target, spell)
     end 
 
     return xi.spells.damage.useDamageSpell(caster, target, spell)

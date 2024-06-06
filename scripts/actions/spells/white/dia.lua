ae08d623288a34568053665a7388405362d211f3
Michael Everett <michae.everett@curology.com>
2024-06-06 11:17:29 -0700

    Give haste to rdm wit dia

--- scripts/actions/spells/white/dia.lua
+++ scripts/actions/spells/white/dia.lua
@@ -57,8 +57,7 @@ spellObject.onSpellCast = function(caster, target, spell)
     local subJob = caster:getSubJob()
 
     if mainJob == xi.job.RDM or subJob == xi.job.RDM then 
-        local hasteDuration = 120 -- 2 minutes in seconds
-        player:addStatusEffect(xi.effect.HASTE, 30, 3, hasteDuration, 0, 10, 1)
+        player:addStatusEffect(xi.effect.HASTE, hasteAmount, 3, hasteDuration, 0, 10, 1)
     end 
     -- Add a 50% chance to cast regen
     if math.random() <= 0.50 then

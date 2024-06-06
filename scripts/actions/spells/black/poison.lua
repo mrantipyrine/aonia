8c11537cd7582a3bb58415983e580a9ba60c2b60
Michael Everett <michae.everett@curology.com>
2024-06-06 10:31:07 -0700

    fungus hat casting bio with poison

--- scripts/actions/spells/black/poison.lua
+++ scripts/actions/spells/black/poison.lua
@@ -11,18 +11,10 @@ spellObject.onSpellCast = function(caster, target, spell)
     local duration = 120
     local mainJob = caster:getMainJob()
     local equippedHead = caster:getEquipID(xi.slot.HEAD)
-    local dotdmg
 
     -- Check if Fungus Hat is equipped
     if equippedHead == 12485 then
-        if mainJob == xi.job.BLM then
-            dotdmg = 20
-        elseif mainJob == xi.job.RDM then
-            dotdmg = 15
-        else
-            return nil
-        end
-
+        dotdmg = (mainJob == xi.job.BLM and 20) or (mainJob == xi.job.RDM and 15) or return nil
         target:addStatusEffect(xi.effect.BIO, dotdmg, 3, duration, 0, 20, 3)
     end
 

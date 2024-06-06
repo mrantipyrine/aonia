b37f18c388476e2e0c4b81d085d7c3cd59bcc1b0
Michael Everett <michae.everett@curology.com>
2024-06-06 10:12:10 -0700

    fungus hat casting bio with poison

--- scripts/actions/spells/black/poison.lua
+++ scripts/actions/spells/black/poison.lua
@@ -9,21 +9,22 @@ end
 
 spellObject.onSpellCast = function(caster, target, spell)
 
-    local equippedBody = caster:getEquipID(xi.slot.BODY)
-    -- Fungus Hat 
-    if equippedBody == 12485 then
-        local duration = 60
-        local dotdmg = 1
-        if skillLvl > 80 then
-            dotdmg = 100000
-        elseif skillLvl > 40 then
-            dotdmg = 20000
-        end
-        target:addStatusEffect(xi.effect.BIO, dotdmg, 3, duration, 0, 20, 3)
-    end 
-    --
-    --
     return xi.spells.enfeebling.useEnfeeblingSpell(caster, target, spell)
 end
+
+local equippedBody = caster:getEquipID(xi.slot.BODY)
+-- Fungus Hat 
+if equippedBody == 12485 then
+    local duration = 60
+    local dotdmg = 1
+    if skillLvl > 80 then
+        dotdmg = 100000
+    elseif skillLvl > 40 then
+        dotdmg = 20000
+    end
+    target:addStatusEffect(xi.effect.BIO, dotdmg, 3, duration, 0, 20, 3)
+end 
+--
+--
 
 return spellObject

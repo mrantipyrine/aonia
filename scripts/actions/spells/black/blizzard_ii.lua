26654facac8dc1e2fa5cddd57c359478898a7bb0
Michael Everett <michael.everett@michaeleverett-mac.attlocal.net>
2024-07-09 13:36:11 -0700

    fixing

--- scripts/actions/spells/black/blizzard_ii.lua
+++ scripts/actions/spells/black/blizzard_ii.lua
@@ -8,31 +8,21 @@ spellObject.onMagicCastingCheck = function(caster, target, spell)
 end
 
 spellObject.onSpellCast = function(caster, target, spell)
-    --local mainJob = player:getMainJob()
-    --local subJob = player:getSubJob()
-    --local hasBuff = player:hasStatusEffect(xi.effect.ICE_SPIKES)
-    --local randomValue = math.random()
-
-    local params = {}
-    params.main = player:getMainJob()
-    params.sub = player:getSubJob()
-    params.buffStatus = player:hasStatusEffect(xi.effect.ICE_SPIKES)
-    params.random = math.random()
-
+    local main = player:getMainJob()
+    local sub = player:getSubJob()
+    local buffStatus = player:hasStatusEffect(xi.effect.ICE_SPIKES)
+    local random = math.random()
     -- 30% increased chance to triple cast if player has Shock Spikes. 
     -- This makes rotations fun
     -- Extend this with items 
-    if params.buffStatus then
+    if buffStatus then
         -- maybe if X item is equipped then X chance to quad cast 
         -- maybe if elemental resistance is > X then quad cast chance
-        if params.main == xi.job.BLM then
-            if params.random <= 0.30 then
-                xi.spells.damage.useDamageSpell(caster, target, spell)
-            end
-        elseif params.random <= 0.10 then
-            xi.spells.damage.useDamageSpell(caster, target, spell)
-            xi.spells.damage.useDamageSpell(caster, target, spell)
-        end
+        if main == xi.job.BLM then
+            if random <= 0.30 then
+                xi.spells.damage.useDamageSpell(caster, target, spell)  
+             end
+        end 
     end
 
     return xi.spells.damage.useDamageSpell(caster, target, spell)

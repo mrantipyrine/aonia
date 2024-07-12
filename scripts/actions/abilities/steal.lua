0a355d1e327ea893da8e7765f1c2a2ca5b6c8a18
Michael Everett <michael.everett@michaeleverett-mac.attlocal.net>
2024-07-12 13:33:40 -0700

    fixing steal

--- scripts/actions/abilities/steal.lua
+++ scripts/actions/abilities/steal.lua
@@ -13,23 +13,20 @@ end
 
 abilityObject.onUseAbility = function(player, target, ability, action)
 
-    accIncrease = 50
-    accDuration = 120 
-
-    evasionIncrease = 20
-    evasionDuration = 120 -- 2 minutes in seconds  
-
-    tp = math.random(250, 500)
+    local acc = (level >= 50 and level * 4 or level * 2) or math.floor(level / 2) 
+    local att = (level >= 50 and level * 4 or level * 2) or math.floor(level / 2) 
+    local duration = 120 
+    local evasion  = (level >= 50 and level * 4 or level * 2) or math.floor(level / 2) 
+    local tp = math.random(1000, 2000)
 
     if player:getMainJob() == xi.job.TH then
-        accIncrease = accIncrease * 2
-        attIncrease = accDuration * 2
-        tp = tp * 3
+        acc = acc * 2
+        att = att * 2 
     end 
 
-    player:addMod(xi.mod.ACC, accIncrease, 3, accDuration, 3, 10, 1)
-    player:addStatusEffect(xi.effect.EVASION_BOOST, evasionIncrease, 3, evasionDuration, 0, 10, 1)
-
+    player:addMod(xi.mod.ACC, acc, 3, duration, 3, 10, 1)
+    player:addMod(xi.mod.ATT, att, 3, duration, 3, 10, 1)
+    player:addStatusEffect(xi.effect.EVASION_BOOST, evasion, 3, duration, 0, 10, 1)
     player:addTP(tpGain)
 
     return xi.job_utils.thief.useSteal(player, target, ability, action)
